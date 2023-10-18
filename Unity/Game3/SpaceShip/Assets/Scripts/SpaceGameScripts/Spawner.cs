2fb5ee3a6f515e5fed14e4a4bcb3518d296f4522
RochoMerino <a00836396@tec.mx>
2023-10-18 12:49:05 -0600

    cambios al background

--- Unity/Game3/SpaceShip/Assets/Scripts/SpaceGameScripts/Spawner.cs
+++ Unity/Game3/SpaceShip/Assets/Scripts/SpaceGameScripts/Spawner.cs
@@ -1,14 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject stars;
+    public GameObject little_star;
+    public GameObject medium_star;
+    public GameObject moon;
     public float maxWidth;
     public float minWidth;
     public float timeToSpawnMin;
     public float timeToSpawnMax;
+    private float lastMoonXPosition = float.MinValue; // Store the last moon's x position
+    public float minMoonDistance = 2.0f; // Minimum distance between consecutive moons
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +23,36 @@ public class Spawner : MonoBehaviour
     {
         yield return new WaitForSeconds(Random.Range(timeToSpawnMin, timeToSpawnMax));
 
-        // Spawn the star at a random x position between minWidth and maxWidth
-        GameObject spawnedStar = Instantiate(stars, new Vector3(transform.position.x + Random.Range(minWidth, maxWidth),
+        // Spawn the little star and medium star as before
+        GameObject spawned_little_star = Instantiate(little_star, new Vector3(transform.position.x + Random.Range(minWidth, maxWidth),
             transform.position.y, 0),
             Quaternion.identity);
 
-        // Destroy the spawned star after 5 seconds
-        Destroy(spawnedStar, 5f);
+        GameObject spawned_medium_star = Instantiate(medium_star, new Vector3(transform.position.x + Random.Range(minWidth, maxWidth),
+            transform.position.y, 0),
+            Quaternion.identity);
+
+        // 10% chance to spawn the moon
+        float moonChance = 0.1f;
+        if (Random.value < moonChance)
+        {
+            float newMoonXPosition = transform.position.x + Random.Range(minWidth, maxWidth);
+
+            // Ensure the new moon doesn't spawn too close to the last one
+            while (Mathf.Abs(newMoonXPosition - lastMoonXPosition) < minMoonDistance)
+            {
+                newMoonXPosition = transform.position.x + Random.Range(minWidth, maxWidth);
+            }
+
+            GameObject spawned_moon = Instantiate(moon, new Vector3(newMoonXPosition, transform.position.y, 0), Quaternion.identity);
+            lastMoonXPosition = newMoonXPosition; // Update the last moon's x position
+
+            Destroy(spawned_moon, 5f);
+        }
+
+        // Destroy the spawned stars after 5 seconds
+        Destroy(spawned_little_star, 5f);
+        Destroy(spawned_medium_star, 5f);
 
         StartCoroutine(SpawnerTimer());
     }
