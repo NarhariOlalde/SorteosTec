48e537bec454124dc3bdc45babdfef9f29c1ff1a
Daniel Muñoz Lozano <a01721797@tec.mx>
2023-11-20 16:34:55 -0600

    Nueva branch para evitar conflictos con lo que sea que Óscar esté haciendo

--- Unity/Game3/SpaceShip/Assets/Scripts/SpaceGameScripts/UIController.cs
+++ Unity/Game3/SpaceShip/Assets/Scripts/SpaceGameScripts/UIController.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     public Text ScoreText;
+    public Sprite SpentLife;
+    public Image[] livesImage;
 
     // Start is called before the first frame update
     void Awake()
     {
+	UpdateLives();
     }
 
     // Start is called before the first frame update
@@ -23,6 +26,30 @@ public class UIController : MonoBehaviour
     {
 	int currentScore = PlayerPrefs.GetInt("score", 0);
         ScoreText.text = "Score: " + currentScore.ToString();
+    }
+
+    public void UpdateLives()
+    {
+	int lives = PlayerPrefs.GetInt("lives", 3);
+	if (lives == 3)
+	{
+
+	}
+	else if (lives == 2)
+	{
+	    livesImage[2].sprite = SpentLife;
+	}
+	else if (lives == 1)
+	{
+	    livesImage[2].sprite = SpentLife;
+	    livesImage[1].sprite = SpentLife;
+	}
+	else if (lives == 0)
+	{
+	    livesImage[2].sprite = SpentLife;
+	    livesImage[1].sprite = SpentLife;
+	    livesImage[0].sprite = SpentLife;
+	}
     }
 }
 
