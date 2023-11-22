36c5502b9745b70e07288fc20d5030838280709d
Osclutch <a00836391@tec.mx>
2023-11-22 16:08:41 -0600

    Anadidos los botones y los coloeres correctos

--- Unity/Game3/SpaceShip/Assets/Game1/Scripts/ShopManagerScript.cs
+++ Unity/Game3/SpaceShip/Assets/Game1/Scripts/ShopManagerScript.cs
@@ -22,9 +22,9 @@ public class ShopManagerScript : MonoBehaviour
         shopItems[1, 2] = 2; // ID for Loot Box 2
         shopItems[1, 3] = 3;
 
-        shopItems[2, 1] = 10000; // Price for Loot Box 1
-        shopItems[2, 2] = 20000; // Price for Loot Box 2
-        shopItems[2, 3] = 30000; // Price for Loot Box 2
+        shopItems[2, 1] = 1; // Price for Loot Box 1
+        shopItems[2, 2] = 2; // Price for Loot Box 2
+        shopItems[2, 3] = 3; // Price for Loot Box 2
     }
 
     public void Buy()
