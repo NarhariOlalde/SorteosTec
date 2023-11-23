635c30912a1e3e54cd3fa5e87c3fb4fb7c2acd38
Daniel Muñoz Lozano <a01721797@tec.mx>
2023-11-22 23:38:35 -0600

    Finished pause and added animation to end scene.

--- Unity/Game3/SpaceShip/Assets/Scripts/GameOverScripts/GameOverUIController.cs
+++ Unity/Game3/SpaceShip/Assets/Scripts/GameOverScripts/GameOverUIController.cs
@@ -7,12 +7,22 @@ using JetBrains.Annotations;
 
 public class GameOverUIController : MonoBehaviour
 {
+    public Animator corazon1_animator;
+    public Animator corazon2_animator;
+    public Animator corazon3_animator;
     public Text puntajeFinal;
     public AudioClip loseSound;
     private AudioSource[] allAudioSources;
+    public Sprite SpentLife;
+    public Image[] livesImage;
+
     // Start is called before the first frame update
     void Start()
     {
+	// DEBUG LINES, DELETE LATER
+	//PlayerPrefs.SetInt("lives", 3);
+	//animator.SetTrigger("FadeLife3");
+	//corazon3_animator.SetTrigger("FadeLife3");
         allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
         foreach (AudioSource audioS in allAudioSources)
         {
@@ -26,6 +36,8 @@ public class GameOverUIController : MonoBehaviour
         puntajeFinal.text = "PUNTOS TOTALES: " + currentScore.ToString();
         PlayerPrefs.SetInt("score", 0);
         PlayerPrefs.SetInt("lives", PlayerPrefs.GetInt("lives", 3) - 1);
+	FadeLives();
+
     }
 
     // Update is called once per frame
@@ -37,5 +49,52 @@ public class GameOverUIController : MonoBehaviour
     public void GoHome()
     {
         SceneManager.LoadSceneAsync("MainMenu");
+    }
+
+    public void FadeLives()
+    {
+	int lives = PlayerPrefs.GetInt("lives", 3);
+	if (lives == 3)
+	{
+	}
+	else if (lives == 2)
+	{
+	    corazon3_animator.SetTrigger("FadeLife");
+	}
+	else if (lives == 1)
+	{
+	    livesImage[2].sprite = SpentLife;
+	    corazon2_animator.SetTrigger("FadeLife");
+	}
+	else if (lives == 0)
+	{
+	    livesImage[2].sprite = SpentLife;
+	    livesImage[1].sprite = SpentLife;
+	    corazon1_animator.SetTrigger("FadeLife");
+	}
+    }
+
+    public void UpdateLives()
+    {
+	int lives = PlayerPrefs.GetInt("lives", 3);
+	if (lives == 3)
+	{
+
+	}
+	else if (lives == 2)
+	{
+	    //livesImage[2].sprite = SpentLife;
+	}
+	else if (lives == 1)
+	{
+	    livesImage[2].sprite = SpentLife;
+	    //livesImage[1].sprite = SpentLife;
+	}
+	else if (lives == 0)
+	{
+	    livesImage[2].sprite = SpentLife;
+	    //livesImage[1].sprite = SpentLife;
+	    //livesImage[0].sprite = SpentLife;
+	}
     }
 }
