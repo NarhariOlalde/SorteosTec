d6b2c98ee9463418d15fe2c359ab0ae9ec11a3f7
Carlos <A00836623@tec.mx>
2023-11-28 01:14:47 -0600

    Cookies added to web

--- Web/Web/Pages/Logout.cshtml.cs
+++ Web/Web/Pages/Logout.cshtml.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Web.Pages
 {
-	public class LogoutModel : PageModel
+    public class LogoutModel : PageModel
     {
         public IActionResult OnGet()
         {
             HttpContext.Session.Clear();
+            foreach (var cookie in HttpContext.Request.Cookies)
+            {
+                HttpContext.Response.Cookies.Delete(cookie.Key);
+            }
             return RedirectToPage("/Index");
         }
     }
